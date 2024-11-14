909b262a823fb1fae2e15cdd81ddb0d573f3ce4d
Saleem-ASD <saleem.amiri@student.uts.edu.au>
2024-11-14 20:31:13 +1100

    Created In game UI Appearance including HUD and Ghost Labels. 60% band

--- Assets/Scripts/StartMenuController.cs
+++ Assets/Scripts/StartMenuController.cs
@@ -5,12 +5,17 @@ public class StartMenuController : MonoBehaviour
 {
     public void LoadLevel1()
     {
-        SceneManager.LoadScene("13921301_Assess3"); 
+        SceneManager.LoadScene("13921301_Assess3");
     }
 
     public void LoadLevel2()
     {
-        SceneManager.LoadScene("DesignIterationScene"); 
+        SceneManager.LoadScene("DesignIterationScene");
+    }
+    
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("Start Scene");
     }
 }
 
