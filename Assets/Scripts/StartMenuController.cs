28fcc911f2d063ae38dda76a7c1834c74cb84a3d
Saleem-ASD <saleem.amiri@student.uts.edu.au>
2024-11-14 18:46:34 +1100

    Start screen update

---
+++ Assets/Scripts/StartMenuController.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenuController : MonoBehaviour
+{
+    public void LoadLevel1()
+    {
+        SceneManager.LoadScene("13921301_Assess3"); 
+    }
+
+    public void LoadLevel2()
+    {
+        SceneManager.LoadScene("DesignIterationScene"); 
+    }
+}
+
